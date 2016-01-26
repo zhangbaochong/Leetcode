85444c9de3a9b83f98db6cdd5fd7da9c04a41150
zhangbaochong <1004038545@qq.com>
2016-01-26 14:15:47 +0800

    Linked List

--- Leetcode/Partition List.cpp
+++ Leetcode/Partition List.cpp
@@ -1,11 +1,11 @@
 /**
-* Definition for singly-linked list.
 * struct ListNode {
 *     int val;
 *     ListNode *next;
 *     ListNode(int x) : val(x), next(NULL) {}
 * };
 */
+
 class Solution {
 public:
 	ListNode* partition(ListNode *&head, int x) {
