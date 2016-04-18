255cf733311221bacaf257a70f7b89ab3175a7a6
zhangbaochong <1004038545@qq.com>
2016-04-18 20:56:12 +0800

    Array Greedy

---
+++ Leetcode/Jump Game.cpp
@@ -0,0 +1,23 @@
+/*Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Determine if you are able to reach the last index.
+
+For example:
+A = [2,3,1,1,4], return true.
+
+A = [3,2,1,0,4], return false.*/
+class Solution {
+public:
+	bool canJump(vector<int>& nums) {
+		int dis = 0;
+		for (int i = 0; i <= dis; ++i)
+		{
+			dis = max(dis, i + nums[i]);
+			if (dis >= nums.size() - 1)
+				return true;
+		}
+		return false;
+	}
+};
